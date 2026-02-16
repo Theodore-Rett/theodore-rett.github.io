63e39c1c827a1f40825f80f35af658f07302ff0a
Theodore-Rett <theodore.rett@gmail.com>
2026-02-16 15:55:45 -0600

    modified about me

--- data/projects.js
+++ data/projects.js
@@ -6,7 +6,7 @@ export const projects = [
 		year: "2023",
 		description: "Created a custom recreation of the popular New York Times game, Wordle, using Java. Worked in a small team of three people utilizing a scrum workflow to design and build the game from the ground up.",
 		stack: ["Java", "JavaFx", "CSS"],
-        repo: ""
+        repo: "https://github.com/Theodore-Rett/desktop-wordle"
 	},
 	{
 		slug: "bartender",
@@ -14,7 +14,7 @@ export const projects = [
 		environment: "MSOE - Junior Project",
 		year: "2024 - 2025",
 		description:
-			"ToDo",
+			"A virtual recipe book and bartender assistant. Users can upload drink recipes via a csv file, and then browse and search through large collections of drinks, ingredients, and different cocktails",
 		stack: ["React", "JavaScript", "SQL", "Node.js"],
         repo: "https://github.com/Theodore-Rett/bartender"
 	},
@@ -24,7 +24,7 @@ export const projects = [
 		environment: "MSOE - Capstone",
 		year: "2025-2026",
 		description:
-			"ToDo",
+			"A educational web based game, designed to help students engage with one another and learn team building and communication skills. Students will work together to pilot a spaceship from one destination to the next, playing a series of mini games to make progress and gain resources.",
 		stack: ["Godot", "GDScript", "Figma"],
         repo: ""
 	},
@@ -34,7 +34,7 @@ export const projects = [
 		environment: "MSOE - SWE 4411",
 		year: "2026",
 		description:
-			"ToDo",
+			"A 2D dungron crawler adventure game built in Godot. Following the journery of a young man who dreams of becoming a knight. Players will explore the world, battling monsters, and uncovering the ancient plot to rule the world.",
 		stack: ["Godot", "GDScript"],
 		repo: "https://github.com/Theodore-Rett/night-quest"
 	}
--- pages/About.jsx
+++ pages/About.jsx
@@ -1,25 +1,37 @@
 export default function About() {
 	return (
 		<>
-		    <div>
-				<img></img>
-				<h1>Theodore Rett</h1>
-				<h3>Software Engineer | MSOE Class of 26</h3>
+		    <div className="about-hero">
+				<img
+					className="about-hero__image"
+					src="/headshot.jpg"
+					alt="Theo headshot"
+				/>
+				<div className="about-hero__text">
+					<h1 className="about-hero__title">Theodore Rett</h1>
+					<h3 className="about-hero__subtitle">About Me</h3>
+					<p className="about-hero__role">
+						Software Engineer | MSOE Class of 26
+					</p>
+				</div>
 			</div>
 
 			<div>
-				<h1>Proffestional Work</h1>
-				<p>Theo has a lot of experience working in team environments, spending 7 years in technical theater, working behind the stage to coordinate props, set, costumes, along with the cast and crew of productions. Not only that but his education at the Milwaukee School of Engineering has him regularly working in team environments, using agile development techniques to develop software.</p>
-				<p>He is a Software Engineering sophomore with a minor in User Experience at the Milwaukee School of Engineering. Studying various design protocols and expectations for both front and back end development. Working towards making programs that are not only functional but easy and intuitive to use.</p>
+				<h1>Professional Work</h1>
+				<p>Theo is currently attending the Milwaukee School of Engineering as a Senior Software Engineer, with a minor in user experience, and is graduating in this May. During his time at MSOE he has had extensive work in team environments working in Agile based workflows for application development. Additionally he is currently working as an Application Development Intern at Brady Corporation, working primarily with React doing web based development.</p>
+				<p>He has a passion for website and game development, with lots of full stack engineering experience. Combining both his software and UX experience to create applications that aren't just functional but intuitive and easy to navigate.</p>
 			</div>
 			<div className="text-holder">
 				<div>
 					<h1>Interests and Hobbies</h1>
-					<p>Between classes and assignments Theo enjoys hanging out with his friends playing Dungeons and Dragons and Magic the Gathering</p>
-					<p>He is active in MSOE Theatre Troupe, and is currently the Production President for the club. He has directed shows like the Lightning Thief, Clue on Stage, and Putnam County’s 25th Annual Spelling Bee.</p>
+					<p>In his free time Theo enjoys spending time with his friends. Playing games like Dungeons and Dragons and Magic the Gathering, going to conventions and the yearly renaissance festival, and climbing mountains together in the video game Peak.</p>
+					<p>Theo has also had eight years of experience doing technical theater. He has served as the Production President of MSOE's Theater Troupe, for two years. As well as directed shows such as The 25th Annual Putnam County Spelling Bee, Clue, and The Lightning Thief.</p>
 				</div>
 				<div id="image-cluster">
-
+					<img src="/first_day.jpg" alt="Theo's at MSOE's last first day event his senior year"></img>
+					<img src="/hamilton_performance.jpg" alt="Theo performing in MSOE's caberate"></img>
+					<img src="/magic_con.jpg" alt="Theo at Magic Con Chicago 2025"></img>
+					<img src="/super_knight.jpg" alt="Theo dressed up for Renisance Fair 2025"></img>
 				</div>
 			</div>
 		</>
